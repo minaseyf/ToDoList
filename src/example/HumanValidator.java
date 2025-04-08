f6fcb175856aae4e65705b6ba6a1374b85c1ea8a
mina <mina.seyff@gmail.com>
2025-04-08 18:05:46 +0330

    implement humanValidator class.

--- src/example/HumanValidator.java
+++ src/example/HumanValidator.java
@@ -1,4 +1,19 @@
 package example;
 
-public class HumanValidator {
+import db.Entity;
+import db.Validator;
+import db.exception.InvalidEntityException;
+
+public class HumanValidator implements Validator {
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+        if (!(entity instanceof Human))
+            throw new IllegalArgumentException("Entity is not an instance of Human!");
+
+        if (((Human) entity).age < 0)
+            throw new InvalidEntityException("Age can not be negative!");
+
+        if (((Human) entity).name == null || ((Human) entity).name.isEmpty())
+            throw new InvalidEntityException("Name can not be null or empty!");
+    }
 }
