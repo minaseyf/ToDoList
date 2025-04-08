705d31b6f5973ed2b9283b0dc44228c04ab819f4
mina <mina.seyff@gmail.com>
2025-04-08 18:03:48 +0330

    implement InvalidEntityException class.

--- src/db/exception/InvalidEntityException.java
+++ src/db/exception/InvalidEntityException.java
@@ -1,7 +1,7 @@
 package db.exception;
 
-public class InvalidEntityException extends RuntimeException {
-  public InvalidEntityException(String message) {
-    super(message);
-  }
+public class InvalidEntityException extends Exception{
+    public InvalidEntityException(String message) {
+        super(message);
+    }
 }
