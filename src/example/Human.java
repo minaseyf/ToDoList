a86d71f8a69bdb35a3620c25ec70207a7d2de566
mina <mina.seyff@gmail.com>
2025-04-08 18:04:55 +0330

    implement human class.

--- src/example/Human.java
+++ src/example/Human.java
@@ -1,4 +1,27 @@
 package example;
 
-public class Human {
+import db.Entity;
+
+public class Human extends Entity {
+    public static final int HUMAN_ENTITY_CODE = 14;
+    public String name;
+    public int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public Human copy() {
+        Human copyHuman = new Human(name, age);
+        copyHuman.id = id;
+
+        return copyHuman;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return HUMAN_ENTITY_CODE;
+    }
 }
