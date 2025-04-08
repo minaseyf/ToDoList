19f53d25326659170e07e7d57e9423a70b2df5f9
mina <mina.seyff@gmail.com>
2025-04-08 17:57:06 +0330

    implement main class.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import db.Database;
+import db.exception.EntityNotFoundException;
+import db.exception.InvalidEntityException;
+import example.Human;
+import example.HumanValidator;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws InvalidEntityException {
+        Database.registerValidator(Human.HUMAN_ENTITY_CODE, new HumanValidator());
+
+        Human ali = new Human("Ali", -10);
+        Database.add(ali);
     }
 }
